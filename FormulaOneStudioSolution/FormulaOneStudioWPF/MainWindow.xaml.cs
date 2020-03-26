87cd9d374fda8b70cc2a4cf518de1291b1e826ad
Mihai-Canea <51034102+Mihai-Canea@users.noreply.github.com>
2020-03-26 18:02:42 +0100

    Aggiunta la card del team

--- FormulaOneStudioSolution/FormulaOneStudioWPF/MainWindow.xaml.cs
+++ FormulaOneStudioSolution/FormulaOneStudioWPF/MainWindow.xaml.cs
@@ -33,20 +33,35 @@ namespace FormulaOneStudioWPF
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            
+
             db = new DbTools();
             List<CardDriverDLL> driver = new List<CardDriverDLL>();
             driver = db.LoadDrivers();
 
-            for (int i = 0; i < driver.Count; i++)
-            {
-            MyUserControls.CardDriver card = new MyUserControls.CardDriver();
-            card.CardImage = new BitmapImage(new Uri(driver[i].PathImage));
-            card.DriverName = driver[i].Name;
-            card.DriverTeam = driver[i].Team;
-            cardTest.Children.Add(card);
-            }
+            //for (int i = 0; i < driver.Count; i++)
+            //{
+            //    MyUserControls.CardDriver card = new MyUserControls.CardDriver();
+            //    card.CardImage = new BitmapImage(new Uri(driver[i].PathImage));
+            //    card.DriverName = driver[i].Name;
+            //    card.DriverTeam = driver[i].Team;
+            //    cardTest.Children.Add(card);
+            //}
 
+            MyUserControls.CardTeam cardTeamMercedes = new MyUserControls.CardTeam();
+            cardTeamMercedes.CardImage = new BitmapImage(
+                new Uri("https://www.formula1.com/content/dam/fom-website/teams/2020/mercedes.png.transform/4col/image.png"));
+            cardTeamMercedes.TeamName = "Mercedes";
+            cardTeamMercedes.Driver1 = "Lewis Hamilton";
+            cardTeamMercedes.Driver2 = "Valtteri Bottas";
+            cardTest.Children.Add(cardTeamMercedes);
+
+            MyUserControls.CardTeam cardTeamFerrari = new MyUserControls.CardTeam();
+            cardTeamFerrari.CardImage = new BitmapImage(
+                new Uri("https://www.formula1.com/content/dam/fom-website/teams/2020/ferrari.png.transform/4col/image.png"));
+            cardTeamFerrari.TeamName = "Ferrari";
+            cardTeamFerrari.Driver1 = "Sebastian Vettel";
+            cardTeamFerrari.Driver2 = "Charles Leclerc";
+            cardTest.Children.Add(cardTeamFerrari);
         }
     }
 }
